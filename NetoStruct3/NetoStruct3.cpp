65de8a41ce61d10a05c51a6dff0fc19f5c7e5c89
DenisMir <pete4kob12@mail.ru>
2022-08-04 22:46:02 +0300

    work3

---
+++ NetoStruct3/NetoStruct3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+struct Adress
+{
+    std::string country = "Россия";
+    std::string city = "Москва";
+    std::string street = "Арбат";
+    int numHome = 52;
+    int numFlat = 12; //room
+    int numIndex = 142401;
+};
+
+void printAdress(Adress* p)
+{
+    std::cout << "Страна: " << p->country;
+    std::cout << std::endl;
+    std::cout << "Город: " << p->city;
+    std::cout << std::endl;
+    std::cout << "Улица: " << p->street;
+    std::cout << std::endl;
+    std::cout << "Номер дома: " << p->numHome;
+    std::cout << std::endl;
+    std::cout << "Номер квартиры: " << p->numFlat;
+    std::cout << std::endl;
+    std::cout << "Индекс:  " << p->numIndex;
+    std::cout << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Adress p;
+
+    printAdress(&p);
+    p.city = "Осма";
+    printAdress(&p);
+}
