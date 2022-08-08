99164107dbd192cc3855601634815850ee62c6a9
DenisMir <pete4kob12@mail.ru>
2022-08-08 22:49:30 +0300

    work2

--- NetoStruct3/NetoStruct3.cpp
+++ NetoStruct3/NetoStruct3.cpp
@@ -2,29 +2,30 @@
 
 struct Adress
 {
-    std::string country = "Россия";
-    std::string city = "Москва";
-    std::string street = "Арбат";
-    int numHome = 52;
-    int numFlat = 12; //room
-    int numIndex = 142401;
+    std::string country = "";
+    std::string city = "";
+    std::string street = "";
+    int numHome = 0;
+    int numFlat = 0; //room
+    int numIndex = 0;
 };
 
-void printAdress(Adress* p)
+int printAdress(const Adress &p)
 {
-    std::cout << "Страна: " << p->country;
+    std::cout << "Страна: " << p.country;
     std::cout << std::endl;
-    std::cout << "Город: " << p->city;
+    std::cout << "Город: " << p.city;
     std::cout << std::endl;
-    std::cout << "Улица: " << p->street;
+    std::cout << "Улица: " << p.street;
     std::cout << std::endl;
-    std::cout << "Номер дома: " << p->numHome;
+    std::cout << "Номер дома: " << p.numHome;
     std::cout << std::endl;
-    std::cout << "Номер квартиры: " << p->numFlat;
+    std::cout << "Номер квартиры: " << p.numFlat;
     std::cout << std::endl;
-    std::cout << "Индекс:  " << p->numIndex;
+    std::cout << "Индекс:  " << p.numIndex;
     std::cout << std::endl;
     std::cout << std::endl;
+    return 0;
 }
 
 int main()
@@ -32,7 +33,11 @@ int main()
     setlocale(LC_ALL, "Russian");
     Adress p;
 
-    printAdress(&p);
-    p.city = "Осма";
-    printAdress(&p);
+    p = { "Россия" , "Москва" , "Арбат" , 52 , 12 , 142401 };
+
+    printAdress(p);
+
+    p = { "Беларусь" , "Брест" , "Пр-т космонавтов" , 53 , 11 , 42402 };
+
+    printAdress(p);
 }
